8dbdf094fbd71a0ffe27092d2808f19540d87328
harshit120 <harshitkhandelwal2019@gmail.com>
2023-07-29 14:23:15 +0530

    second commit

---
+++ src/containers/Data2.js
@@ -0,0 +1,245 @@
+export const classes =[
+    {
+        subject1:{
+            name: "English",
+            division :{
+             division1: "True",
+             division2: "True",
+             division3: "True",
+             division4: "True",
+            } ,
+        },
+        subject2:{
+            name: "Hindi",
+            division :{
+             division1: "True",
+             division2: "True",
+             division3: "True",
+             division4: "True",
+            } ,
+        },
+        subject3:{
+            name: "Mathematics",
+            division :{
+              division1: "True",
+              division2: "False",
+              division3: "False",
+              division4: "True",
+             } ,
+        },
+        subject4:{
+            name: "Science",
+            division :{
+              division1: "True",
+              division2: "False",
+              division3: "False",
+              division4: "False",
+             } ,
+        },
+        subject5:{
+            name: "Social Science",
+    division :{
+      division1: "True",
+      division2: "True",
+      division3: "True",
+      division4: "True",
+     } ,
+        },
+       
+    },
+    {
+        subject1:{
+            name: "English",
+            division :{
+             division1: "True",
+             division2: "True",
+             division3: "True",
+             division4: "True",
+            } ,
+        },
+        subject2:{
+            name: "Hindi",
+            division :{
+             division1: "True",
+             division2: "True",
+             division3: "True",
+             division4: "True",
+            } ,
+        },
+        subject3:{
+            name: "Mathematics",
+            division :{
+              division1: "True",
+              division2: "False",
+              division3: "False",
+              division4: "True",
+             } ,
+        },
+        subject4:{
+            name: "Science",
+            division :{
+              division1: "True",
+              division2: "False",
+              division3: "False",
+              division4: "False",
+             } ,
+        },
+        subject5:{
+            name: "Social Science",
+    division :{
+      division1: "True",
+      division2: "True",
+      division3: "True",
+      division4: "True",
+     } ,
+        },
+       
+    },
+
+    {
+        subject1:{
+            name: "English",
+            division :{
+             division1: "True",
+             division2: "True",
+             division3: "True",
+             division4: "True",
+            } ,
+        },
+        subject2:{
+            name: "Hindi",
+            division :{
+             division1: "True",
+             division2: "True",
+             division3: "True",
+             division4: "True",
+            } ,
+        },
+        subject3:{
+            name: "Mathematics",
+            division :{
+              division1: "True",
+              division2: "False",
+              division3: "False",
+              division4: "True",
+             } ,
+        },
+        subject4:{
+            name: "Science",
+            division :{
+              division1: "True",
+              division2: "False",
+              division3: "False",
+              division4: "False",
+             } ,
+        },
+        subject5:{
+            name: "Social Science",
+    division :{
+      division1: "True",
+      division2: "True",
+      division3: "True",
+      division4: "True",
+     } ,
+        },
+       
+    },
+    {
+        subject1:{
+            name: "English",
+            division :{
+             division1: "True",
+             division2: "True",
+             division3: "True",
+             division4: "True",
+            } ,
+        },
+        subject2:{
+            name: "Hindi",
+            division :{
+             division1: "True",
+             division2: "True",
+             division3: "True",
+             division4: "True",
+            } ,
+        },
+        subject3:{
+            name: "Mathematics",
+            division :{
+              division1: "True",
+              division2: "False",
+              division3: "False",
+              division4: "True",
+             } ,
+        },
+        subject4:{
+            name: "Science",
+            division :{
+              division1: "True",
+              division2: "False",
+              division3: "False",
+              division4: "False",
+             } ,
+        },
+        subject5:{
+            name: "Social Science",
+    division :{
+      division1: "True",
+      division2: "True",
+      division3: "True",
+      division4: "True",
+     } ,
+        },
+       
+    },
+    {
+        subject1:{
+            name: "English",
+            division :{
+             division1: "True",
+             division2: "True",
+             division3: "True",
+             division4: "True",
+            } ,
+        },
+        subject2:{
+            name: "Hindi",
+            division :{
+             division1: "True",
+             division2: "True",
+             division3: "True",
+             division4: "True",
+            } ,
+        },
+        subject3:{
+            name: "Mathematics",
+            division :{
+              division1: "True",
+              division2: "False",
+              division3: "False",
+              division4: "True",
+             } ,
+        },
+        subject4:{
+            name: "Science",
+            division :{
+              division1: "True",
+              division2: "False",
+              division3: "False",
+              division4: "False",
+             } ,
+        },
+        subject5:{
+            name: "Social Science",
+    division :{
+      division1: "True",
+      division2: "True",
+      division3: "True",
+      division4: "True",
+     } ,
+        },
+       
+    }
+
+    
+]
